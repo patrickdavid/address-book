b594c2e89b31c9373708fabe6f9018f779f08235
moof <moofmayeda@yahoo.com>
2014-07-15 11:05:51 -0700

    updated address book to include multiple addresses

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,46 @@
 $(document).ready (function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                '<div class="form-group">' +
+                                '<label for="new-street">Street</label>' +
+                                '<input type="text" class="form-control new-street">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                '<label for="new-city">City</label>' +
+                                '<input type="text" class="form-control new-city">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                '<label for="new-state">State</label>' +
+                                '<input type="text" class="form-control new-state">' +
+                                '</div>' +
+                                '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
-    console.log(inputtedAddress);
-    var contact = {firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress};
-    $("ul").append("<li>"+contact.firstName+ " "+contact.lastName+"</li>");
+    var contact = {firstName: inputtedFirstName, lastName: inputtedLastName, addresses: []};
+    $(".new-address").each(function () {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var inputtedAddress = {street: inputtedStreet, city: inputtedCity, state: inputtedState};
+      contact.addresses.push(inputtedAddress);
+    });
+    $("ul#contactList").append("<li class='contactEntry'>"+contact.firstName+ " "+contact.lastName+"</li>");
     $("#show-contacts").show();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    $("input#new-address").val("");
-    $("li").last().click(function() {
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+    $("li.contactEntry").last().click(function() {
       $("#show-details").show();
       $("#first-name").text(contact.firstName);
       $("#last-name").text(contact.lastName);
-      $("#address").text(contact.address);
+      $("ul#addressEntries").empty();
+      contact.addresses.forEach(function(address) {
+        $("ul#addressEntries").append("<li>"+address.street + ", " + address.city + ", " + address.state + "</li>");
+      });
     });
     event.preventDefault();
   });
